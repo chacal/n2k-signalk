16a2e26a0f536051647ed981cadc49ea6978ceaa
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2017-07-23 10:45:10 +0300

    Add -nv input test for PGN 127488

--- test/127488_engine_speed.js
+++ test/127488_engine_speed.js
@@ -1,13 +1,14 @@
+var NV = require('./util/nv_input_test')
 var chai = require('chai')
 chai.Should()
 chai.use(require('chai-things'))
 chai.use(require('signalk-schema').chaiModule)
 
-describe('127488 engine speed Port', function () {
+describe('127488 engine speed Port', NV.test(function (nv) {
   it('complete engine speed sentence converts', function () {
     var tree = require('../n2kMapper.js').toNested(
       JSON.parse(
-        '{"timestamp":"2015-01-15-16:16:56.749Z","prio":"2","src":"17","dst":"255","pgn":"127488","description":"Engine Parameters, Rapid Update","fields":{"Engine Instance":"Single Engine or Dual Engine Port","Engine Speed":"3190"}}'
+        `{"timestamp":"2015-01-15-16:16:56.749Z","prio":"2","src":"17","dst":"255","pgn":"127488","description":"Engine Parameters, Rapid Update","fields":{"Engine Instance":${NV.F(nv, "Single Engine or Dual Engine Port", 0)},"Engine Speed":"3190"}}`
       )
     )
     tree.should.have.nested.property('propulsion.port.revolutions')
@@ -17,13 +18,13 @@ describe('127488 engine speed Port', function () {
     )
     tree.should.be.validSignalKVesselIgnoringIdentity
   })
-})
+}))
 
-describe('127488 engine speed Starboard', function () {
+describe('127488 engine speed Starboard', NV.test(function (nv) {
   it('complete engine speed sentence converts', function () {
     var tree = require('../n2kMapper.js').toNested(
       JSON.parse(
-        '{"timestamp":"2015-01-15-16:16:56.749Z","prio":"2","src":"17","dst":"255","pgn":"127488","description":"Engine Parameters, Rapid Update","fields":{"Engine Instance":"Dual Engine Starboard","Engine Speed":"3190"}}'
+        `{"timestamp":"2015-01-15-16:16:56.749Z","prio":"2","src":"17","dst":"255","pgn":"127488","description":"Engine Parameters, Rapid Update","fields":{"Engine Instance":${NV.F(nv, "Dual Engine Starboard", 1)},"Engine Speed":"3190"}}`
       )
     )
     tree.should.have.nested.property('propulsion.starboard.revolutions')
@@ -33,4 +34,4 @@ describe('127488 engine speed Starboard', function () {
     )
     tree.should.be.validSignalKVesselIgnoringIdentity
   })
-})
+}))
