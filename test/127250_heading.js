00b6f056200a23170b11251eea9df190ca7de560
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2017-07-23 10:44:45 +0300

    Create a helper for constructing -nv fields to avoid typing errors with JSON strings

--- test/127250_heading.js
+++ test/127250_heading.js
@@ -1,13 +1,14 @@
+var NV = require('./util/nv_input_test')
 var chai = require('chai')
 chai.Should()
 chai.use(require('chai-things'))
 chai.use(require('signalk-schema').chaiModule)
 
-describe('127250 Heading', require('./util/nv_input_test')(function (nv) {
+describe('127250 Heading', NV.test(function (nv) {
   it('Magnetic sentence converts', function () {
     var tree = require('../n2kMapper.js').toNested(
       JSON.parse(
-        `{"timestamp":"2013-10-08-15:47:28.263Z","prio":"2","src":"204","dst":"255","pgn":"127250","description":"Vessel Heading","fields":{"Heading":"129.7","Reference":${nv ? '{"value":1,"name":"Magnetic"}' : '"Magnetic"'}}}`
+        `{"timestamp":"2013-10-08-15:47:28.263Z","prio":"2","src":"204","dst":"255","pgn":"127250","description":"Vessel Heading","fields":{"Heading":"129.7","Reference":${NV.F(nv, 'Magnetic', 1)}}}`
       )
     )
     tree.should.have.with.nested.property(
@@ -20,7 +21,7 @@ describe('127250 Heading', require('./util/nv_input_test')(function (nv) {
   it('Variation sentence converts', function () {
     var tree = require('../n2kMapper.js').toNested(
       JSON.parse(
-        `{"timestamp":"2013-10-08-15:47:28.264Z","prio":"2","src":"1","dst":"255","pgn":"127250","description":"Vessel Heading","fields":{"SID":"68","Variation":"8.0","Reference":${nv ? '{"value":1,"name":"True"}' : '"True"'}}}`
+        `{"timestamp":"2013-10-08-15:47:28.264Z","prio":"2","src":"1","dst":"255","pgn":"127250","description":"Vessel Heading","fields":{"SID":"68","Variation":"8.0","Reference":${NV.F(nv, 'True', 1)}}}`
       )
     )
     tree.should.have.with.nested.property(
@@ -33,7 +34,7 @@ describe('127250 Heading', require('./util/nv_input_test')(function (nv) {
   it('True heading sentence converts', function () {
     var tree = require('../n2kMapper.js').toNested(
       JSON.parse(
-        `{"timestamp":"2016-04-14T09:34:11.275Z","prio":2,"src":2,"dst":255,"pgn":127250,"description":"Vessel Heading","fields":{"Heading":14.8,"Deviation":0.0,"Variation":0.0,"Reference":${nv ? '{"value":1,"name":"True"}' : '"True"'}}}`
+        `{"timestamp":"2016-04-14T09:34:11.275Z","prio":2,"src":2,"dst":255,"pgn":127250,"description":"Vessel Heading","fields":{"Heading":14.8,"Deviation":0.0,"Variation":0.0,"Reference":${NV.F(nv, 'True', 1)}}}`
       )
     )
     tree.should.have.with.nested.property('navigation.headingTrue.value', 14.8)
--- test/util/nv_input_test.js
+++ test/util/nv_input_test.js
@@ -1,9 +1,14 @@
-module.exports = function nvModeTest(tests) {
-  return function() {
-    [false, true].forEach(function(nvMode) {
-      describe(`In ${nvMode ? "'-nv'" : 'normal'} input mode`, function() {
-        tests(nvMode)
+module.exports = {
+  test: function nvModeTest(tests) {
+    return function() {
+      [false, true].forEach(function(nvMode) {
+        describe(`In ${nvMode ? "'-nv'" : 'normal'} input mode`, function() {
+          tests(nvMode)
+        })
       })
-    })
+    }
+  },
+  F: function nvField(nv, name, value) {
+    return nv ? `{"name":"${name}","value":${value}}` : `"${name}"`
   }
 }
